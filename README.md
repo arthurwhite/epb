e6dae34f4ed1b32d01688be43eb2297ce160303e
Arthur White <arthur@white.li>
2018-11-20 12:18:35 +0100

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Energy performance of buildings
+
+This library provides helpers for energy performance of buildings computation.
+
+## Installing
+
+```sh
+npm i epb
+```
+
+## Usage
+
+### `energyClass`
+
+Get the energy class (like `A++`, `A+`, `A`, `B`...) following the regulator rules, given the energy consumption.  
+If the energy class cannot be calculated from the provided data, `null` is returned.
+
+```ts
+const consumption = 100;
+const ecls = await energyClass(Regulator.Brussels, consumption);
+// ecls === "C+"
+```
+
+### Regulators
+
+Currently supported:
+
+-   `brussels`
+-   `flanders` (_exception: energy classes are not used in Flanders_)
+-   `france`
+-   `wallonia`
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+export enum Regulator {
+    Brussels = "brussels",
+    Flanders = "flanders",
+    France = "france",
+    Wallonia = "wallonia",
+}
+
+/**
+ * Get the energy class (like `A++`, `A+`, `A`, `B`...) following the regulator rules, given the energy consumption.
+ * If the energy class cannot be calculated, `null` is returned.
+ *
+ * @param regulator The regulator that defines class calculation rules.
+ * @param consumption The consumption of the building in kWh/m².year.
+ */
+export async function energyClass(regulator: Regulator, consumption: number) {
+    switch (regulator) {
+        // tslint:disable:curly
+        case Regulator.Brussels:
+            if (consumption <= 0) return "A++";
+            else if (consumption <= 15) return "A+";
+            else if (consumption <= 30) return "A";
+            else if (consumption <= 45) return "A-";
+            else if (consumption <= 62) return "B+";
+            else if (consumption <= 78) return "B";
+            else if (consumption <= 95) return "B-";
+            else if (consumption <= 113) return "C+";
+            else if (consumption <= 132) return "C";
+            else if (consumption <= 150) return "C-";
+            else if (consumption <= 170) return "D+";
+            else if (consumption <= 190) return "D";
+            else if (consumption <= 210) return "D-";
+            else if (consumption <= 232) return "E+";
+            else if (consumption <= 253) return "E";
+            else if (consumption <= 275) return "E-";
+            else if (consumption <= 345) return "F";
+            else return "G";
+
+        case Regulator.France:
+            if (consumption <= 50) return "A";
+            else if (consumption <= 90) return "B";
+            else if (consumption <= 150) return "C";
+            else if (consumption <= 230) return "D";
+            else if (consumption <= 330) return "E";
+            else if (consumption <= 450) return "F";
+            else return "G";
+
+        case Regulator.Wallonia:
+            if (consumption <= 0) return "A++";
+            else if (consumption <= 45) return "A+";
+            else if (consumption <= 85) return "A";
+            else if (consumption <= 170) return "B";
+            else if (consumption <= 255) return "C";
+            else if (consumption <= 340) return "D";
+            else if (consumption <= 425) return "E";
+            else if (consumption <= 510) return "F";
+            else return "G";
+
+        default:
+            return null;
+        // tslint:enable:curly
+    }
+}
